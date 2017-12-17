4765efd4a9a221e46d444acd5670b14a1f72bc8f
Keerten <keertenk@gmail.com>
2017-12-17 08:13:28 +0000

    Bring Up for XenonHD
    Signed-off-by: Keerten <keertenk@gmail.com>

--- xenonhd.mk
+++ xenonhd.mk
@@ -1,5 +1,6 @@
 # Copyright (C) 2015 The CyanogenMod Project
 #           (C) 2017 The LineageOS Project
+#           (C) 2017 The TeamHorizon Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit some common LineageOS stuff.
+# Inherit some common XenonHD stuff.
 
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
@@ -28,7 +29,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 ## Device identifier. This must come after all inclusions
 
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := lineage_athene
+PRODUCT_NAME := xenonhd_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
@@ -38,3 +39,7 @@ PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
        PRODUCT_NAME="Moto G4/Plus"
+
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.xenonhd.maintainer="Keerten"
